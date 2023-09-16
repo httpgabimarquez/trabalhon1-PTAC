78a173d1dccd7fd845af9fc9b01062db9b326425
gabimarques0 <gabrielamarquezzzz6@gmail.com>
2023-09-16 19:26:36 -0400

    dashboard

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,11 +1,15 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { Suspense } from "react";
+import ListUsers from "@/app/components/ListUsers";
 
 export default async function Dashboard() {
-   
+   const users = getUsers()
     return (
         <div>
             <h1>Dashboard</h1>
+        <Suspense fallback={<p>Carregando os dados da página...</p>}>
             
+        </Suspense>
         </div>
     );
 };

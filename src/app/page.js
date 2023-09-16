37aa0897f0dec5ba0d3421bfbe06cfdc54032546
gabimarques0 <gabrielamarquezzzz6@gmail.com>
2023-09-16 19:43:03 -0400

    Correcao de erros - page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function Login() {
   const [user, setUser] = useState({
@@ -13,28 +15,40 @@ export default function Login() {
   const handlerLogin = async (e) => {
     e.preventDefault();
     try {
-      await handlerAcessUser(user);
+     const userAuth = await handlerAcessUser(user);
+     if(userAuth.token === undefined){
+      toast.error("Ocorreu um erro no e-mail ou senha, por favor verifique!")
+     }
       push('/pages/dashboard');
     } catch {
       refresh();
     }
   }
+
   return (
-    <div>
-      <h1>Login</h1>
+    <body>
+      <div className="body">
       <form onSubmit={handlerLogin}>
+        <div>
+
+        </div>
+        <h1>Entre!</h1>
         <input
           placeholder='E-mail'
           type="email"
           onChange={(e) => { setUser({ ...user, email: e.target.value }) }}>
         </input>
+
         <input
           placeholder='Senha'
           type='password'
           onChange={(e) => { setUser({ ...user, password: e.target.value }) }}>
         </input>
+
         <button>Entrar</button>
       </form>
+      <ToastContainer/>
     </div>
-  )
+    </body>
+  );
 }

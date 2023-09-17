3f7046c7cddb66bce7de9468aa3c64785f9c1c21
gabimarques0 <gabrielamarquezzzz6@gmail.com>
2023-09-16 21:53:47 -0400

    classname

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -5,8 +5,8 @@ import ListUsers from "@/app/components/ListUsers";
 export default async function Dashboard(){
    const users = getUsers()
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="box-title">
+            <h1 className="title">Dashboard</h1>
         <Suspense fallback={<p>Carregando os dados da página...</p>}>
             <ListUsers users={users}/>
         <button><a href ="/pages/alter">Alterar</a></button>

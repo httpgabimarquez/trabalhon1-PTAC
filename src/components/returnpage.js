a8841fa45cc8c0d9fa26de9507bccec99163074d
bolinhaebolao <gabrielamarquezzzz6@gmail.com>
2023-09-17 17:38:46 -0400

    return page

---
+++ src/components/returnpage.js
@@ -0,0 +1,15 @@
+'use client'
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
+export default function Returnlog(){
+    const{push} = useRouter();
+    const retorne = (e) => {
+        e.preventDefault();
+        Cookies.remove('token')
+        push('/')
+    }
+    return(
+        <div> <button onClick={retorne}>Retornar</button></div>
+    )
+}

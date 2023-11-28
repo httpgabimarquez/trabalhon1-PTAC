8210810f4bc63e2c8663da8a54a0d4e10630fcae
gabrielamarques <gabrielamarquezzzz6@gmail.com>
2023-11-28 15:02:34 -0400

    rotas para acessar o alterar

--- src/components/ListUsers.js
+++ src/components/ListUsers.js
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 export default async function ListUsers({users}){
     await new Promise((resolve) => setTimeout(resolve, 3000));
     return(
         <div>
             {users?.map((user, index) =>
             <p key={index}>
-                {user.name}
+                 <Link href={`/pages/dashboard/alter/${user.id}`}> {user.name}</Link>
+               
             </p>
-            )}
+        )}
         </div>
+   
     )
 }
